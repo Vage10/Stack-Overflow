6cab76296ae4753bc78d73458394c90e732ec50d
Vageshwari <vageshwari1062@gmail.com>
2023-06-08 17:22:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# `npm install`
+Use the above command to install dependencies at the client and server side 
+
+# `npm start`
+Use the above command to start the client and server side
---
+++ client/src/reducers/currentUser.js
@@ -0,0 +1,10 @@
+const currentUSerReducer = (state = null, action) => {
+    switch(action.type) {
+        case 'FETCH_CURRENT-USER':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default currentUSerReducer
